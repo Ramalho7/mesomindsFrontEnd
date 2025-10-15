ecdae3799800d9af9c2850ea25d670abf772c7b5
Lucas Ramalho <adryelll85@gmail.com>
2025-10-15 00:44:04 -0300

    Add: UserDropDown

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { type StoryFn, type Meta } from '@storybook/react';
 import StyledHeader from './StoryHeader';
+import type { Meta, StoryObj } from '@storybook/react';
 
-export default {
+const meta: Meta<typeof StyledHeader> = {
   title: 'Components/StyledHeader',
   component: StyledHeader,
-} as Meta<typeof StyledHeader>;
-
-const Template: StoryFn<typeof StyledHeader> = (args) => <StyledHeader {...args} />;
-
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  isLoggedIn: true,
 };
 
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {
-  isLoggedIn: false,
+export default meta;
+
+export const LoggedIn: StoryObj<typeof StyledHeader> = {
+  args: {
+    isLoggedIn: true,
+  },
+};
+
+export const LoggedOut: StoryObj<typeof StyledHeader> = {
+  args: {
+    isLoggedIn: false,
+  },
 };
--- src/stories/StoryHeader.tsx
+++ src/stories/StoryHeader.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Button } from '../components/ui/button';
+import React from "react";
+import { Button } from "../components/ui/button";
+import { UserDropdown } from "./UserDropdown";
 
 interface StyledHeaderProps {
   isLoggedIn: boolean;
@@ -7,21 +8,23 @@ interface StyledHeaderProps {
 
 const StyledHeader: React.FC<StyledHeaderProps> = ({ isLoggedIn }) => {
   return (
-    <header className="sticky top-0 bg-white flex justify-between items-center h-[100px] border-b border-action w-full">
-      <div className="logoBrand w-[80px] h-[80px] rounded-full bg-gray"></div>
-      <nav className="flex gap-[3.125rem] text-2xl">
-        <span>Simulados</span>
-        <span>Questões</span>
-        <span>Conteúdos</span>
-        <span>Provas</span>
-        <span>Turmas</span>
-      </nav>
-      {isLoggedIn ? (
-        <Button variant="action">Logout</Button>
-      ) : (
-        <Button variant="action">Entrar</Button>
-      )}
-    </header>
+    <>
+      <header className="sticky top-0 bg-white flex justify-between items-center h-[100px] border-b border-action w-full">
+        <div className="logoBrand w-[80px] h-[80px] rounded-full bg-gray"></div>
+        <nav className="flex gap-[3.125rem] text-2xl">
+          <span>Simulados</span>
+          <span>Questões</span>
+          <span>Conteúdos</span>
+          <span>Provas</span>
+          <span>Turmas</span>
+        </nav>
+        {isLoggedIn ? (
+          <UserDropdown />
+        ) : (
+          <Button variant="action">Entrar</Button>
+        )}
+      </header>
+    </>
   );
 };
 
