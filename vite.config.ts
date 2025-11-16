560c770f4381fd97e1316701a6b31e2e900036a5
Lucas Ramalho <adryelll85@gmail.com>
2025-11-16 16:56:49 -0300

    Add: questions e questionCollection

--- vite.config.ts
+++ vite.config.ts
@@ -4,62 +4,69 @@ import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import { resolve } from 'node:path';
-
-// https://vitejs.dev/config/
+import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'; // Reintroduzindo o plugin para SSR
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [TanStackRouterVite({
-    autoCodeSplitting: true
-  }), viteReact(), tailwindcss()],
+  plugins: [
+    nitroV2Plugin(), /
+    TanStackRouterVite({
+      autoCodeSplitting: true,
+    }),
+    viteReact(),
+    tailwindcss(),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
-    projects: [{
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: 'playwright',
-          instances: [{
-            browser: 'chromium'
-          }]
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          storybookTest({
+            configDir: path.join(dirname, '.storybook'),
+          }),
+        ],
+        test: {
+          name: 'storybook',
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: 'playwright',
+            instances: [
+              {
+                browser: 'chromium',
+              },
+            ],
+          },
+          setupFiles: ['.storybook/vitest.setup.ts'],
         },
-        setupFiles: ['.storybook/vitest.setup.ts']
-      }
-    }]
+      },
+    ],
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src')
-    }
+      '@': resolve(__dirname, './src'),
+    },
   },
   esbuild: {
-    logOverride: { 'console': 'silent' },
+    logOverride: { console: 'silent' },
   },
   define: {
-    'process.env': {}, 
+    'process.env': {},
   },
   optimizeDeps: {
-    exclude: ['console'], 
+    exclude: ['console'],
   },
   build: {
-    ssr: false,
+    ssr: true,
     rollupOptions: {
-      input: './index.html',
+      input: './src/entry-server.tsx', 
     },
     chunkSizeWarningLimit: 1000,
-  }
+  },
 });
