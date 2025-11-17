966fa1d29edfaa06df359cb8e5ed76cc2bf2b4eb
Lucas Ramalho <75459079+Ramalho7@users.noreply.github.com>
2025-11-16 21:09:24 -0300

    Add test (#92)
    * Add: testes simples
    
    * Add: testes simples
    
    * Add: testes simples
    
    * Add: testes simples
    
    * Add: testes e fix login
    
    * Fix: registerForm
    
    * Fix: login form
    
    * Fix: build
    
    * Fix: build
    
    * Fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/mesominds",
   plugins: [
     TanStackRouterVite({
       autoCodeSplitting: true,
